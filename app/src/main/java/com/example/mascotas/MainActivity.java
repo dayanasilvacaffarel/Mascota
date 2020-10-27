7ba8d55627ec0881bb856600743c82165174dcc0
Dayana Silva <dayana.silva.caffarel@gmail.com>
2020-10-27 09:32:19 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mascotas/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mascotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
